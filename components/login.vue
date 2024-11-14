dc647ed04243980372049247b001e32766252c90
JoseEdgarRdzV <je.rodriguezvazquez@ugto.mx>
2024-11-14 16:51:58 -0600

    Cambios en el diseño del Login

--- components/login.vue
+++ components/login.vue
@@ -1,69 +1,77 @@
 <template>
-  <div class="container">
-    <div class="containerLeft">
-      <h2>Logo</h2>
-    </div>
-    <div class="containerRight">
-      <form class="loginForm">
-        <div class="formGroup">
-          <input type="text" placeholder="Ingresa tu usuario">
-        </div>
-        <div class="formGroup">
-          <input type="password" placeholder="Ingresa tu contraseña">
-        </div>
-        <button type="submit">
-          SIGN IN
-        </button>
-      </form>
-    </div>
-  </div>
+  <v-container display="flex" style="height: 100vh; width: 100%;">
+    <v-row style="height: 100%;">
+      <v-col cols="12" sm="6" class="left">
+        <img src="../assets/img/logo.png" alt="Logo" class="logo">
+        <p style="color: white;">
+          WELCOME
+        </p>
+      </v-col>
+
+      <v-col cols="12" sm="6" class="right">
+        <v-card>
+          <v-card-text>
+            <v-form>
+              <v-text-field v-model="email" placeholder="Correo electrónico" label="Correo electrónico" solo />
+              <v-text-field v-model="password" placeholder="Contraseña" label="Contraseña" type="password" solo />
+              <a href="#" style="display: block; text-align: left;">Forgot your password?</a>
+              <v-btn
+                style="background-color: red; color: white;"
+                :disabled="loading"
+                :loading="loading"
+                class="text-none mb-4"
+                color="indigo-darken-3"
+                size="x-large"
+                variant="flat"
+                block
+                @click="loading = !loading"
+              >
+                SIGN IN
+              </v-btn>
+            </v-form>
+          </v-card-text>
+        </v-card>
+      </v-col>
+    </v-row>
+  </v-container>
 </template>
 
 <script>
 export default {
-  name: 'LoginComponent'
+  name: 'LoginComponent',
+  data () {
+    return {
+      email: '',
+      password: ''
+    }
+  }
 }
 </script>
 
-<style scoped>
-.container {
- display: flex;
- width: 100%;
- height: 100vh;
-}
-
-.containerLeft {
-  background-color: red;
+<style>
+.left {
+  background-image: url(../assets/img/FondoLoginR.jpg);
+  background-size: cover;
+  height: 100vh;
   width: 50%;
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
 }
 
-.containerRight {
+.right {
   background-color: blue;
   width: 50%;
   display: flex;
-  justify-content: center;
-  align-items: center;
-}
-
-.loginForm {
-  display: flex;
-  justify-content: center;
-  align-items: center;
-  background-color: white;
   flex-direction: column;
-  width: 50%;
-  height: 50%;
-  text-align: center;
-  margin: 0 auto;
-  border-radius: 7px;
+  justify-content: center;
+  align-items: center;
 }
 
-button {
-  padding: 10px;
-  background-color: red;
-  color: white;
-  border: none;
-  border-radius: 4px;
-  cursor: pointer;
+.logo {
+  max-width: 200px;
+  max-height: 200px;
+
 }
 </style>
